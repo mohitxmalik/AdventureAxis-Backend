a60ec6295eb587712556abd1ce718542241a5a58
Mohit Malik <mohit1630.be20@chitkara.edu.in>
2024-05-01 13:50:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,33 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors')
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 dotenv.config();
 
 // fetching routes
-const authRoute = require('./routes/auth');
-const tourRoute = require('./routes/tours');
-const userRoute = require('./routes/users');
-const reviewRoute = require('./routes/reviews')
-const bookingRoute = require('./routes/bookings');
-const imageRoute = require('./routes/image');
+const authRoute = require("./routes/auth");
+const tourRoute = require("./routes/tours");
+const userRoute = require("./routes/users");
+const reviewRoute = require("./routes/reviews");
+const bookingRoute = require("./routes/bookings");
+const imageRoute = require("./routes/image");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 const corsOptions = {
-    origin: true,
-    credentials: true
-}
+  origin: true,
+  credentials: true,
+};
 
 // connecting database
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 console.log(process.env.MONGO_URI);
 mongoose
-  .connect(
-    "mongodb+srv://mohitxmalik:mongodb123@cluster0.xqkl5pu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(async () => {
     console.log("MongoDB database connected");
   })
@@ -38,14 +35,13 @@ mongoose
     console.log("MongoDB database connection failed", error);
   });
 
-
 // middlewares
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
 
 // routes path and redirecting...
-const routePath = '/api/v1';
+const routePath = "/api/v1";
 app.use(`${routePath}/auth`, authRoute);
 app.use(`${routePath}/tours`, tourRoute);
 app.use(`${routePath}/users`, userRoute);
@@ -55,5 +51,5 @@ app.use(`${routePath}/image`, imageRoute);
 
 // listening at PORT : 5000
 app.listen(PORT, () => {
-    console.log(`server runnin on ${PORT}\nhttp://localhost:${PORT}`);
-})
+  console.log(`server runnin on ${PORT}\nhttp://localhost:${PORT}`);
+});
